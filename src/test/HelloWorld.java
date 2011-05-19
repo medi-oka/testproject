6fea80740e77720346f3ab6179f3db0db08bab0e
medi-oka <oka@mediba.jp>
2011-05-19 17:31:12 +0900

    Test Commit No.1

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package test;
+
+/**
+ * @author oka
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		System.out.println("Hello world!");
+
+	}
+
+}
